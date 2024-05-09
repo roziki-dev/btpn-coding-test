ce9e7f9b016a21864e30b9a9d3181400f2a8a312
Muh. S. Roziki <muh.roziki@gmail.com>
2024-05-09 17:34:22 +0700

    create header

---
+++ src/components/Header/HeaderHome.js
@@ -0,0 +1,30 @@
+import {Text, TextInput, View} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import {styles} from './HeaderHome.style';
+import {color} from '../../styles';
+
+export const HeaderHome = ({title, count}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <View>
+        <View style={styles.search}>
+          <Icon name="search1" size={20} color={color.placeholder} />
+          <TextInput
+            placeholder="Search name here..."
+            selectionColor={color.placeholder}
+            style={styles.searchInput}
+            underlineColorAndroid="transparent"
+            autoCorrect={false}
+            placeholderTextColor={color.placeholder}
+          />
+        </View>
+      </View>
+      <View style={styles.titleBodyContainer}>
+        <Text style={styles.titleBody}>My Contacts ({count})</Text>
+      </View>
+    </View>
+  );
+};
