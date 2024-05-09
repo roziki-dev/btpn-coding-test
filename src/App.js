5e25fd8931b6dc2b520b05fd5e701c43be903d34
Muh. S. Roziki <muh.roziki@gmail.com>
2024-05-09 17:32:57 +0700

    implementation redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {MainRouter} from './router';
+import {store} from './store';
 
 const App = () => {
-  return <MainRouter />;
+  return (
+    <Provider store={store}>
+      <MainRouter />
+    </Provider>
+  );
 };
 
 export default App;
