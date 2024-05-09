c77facc2f5b8f204a645e5c3facce5dfe42d8e87
Muh. S. Roziki <muh.roziki@gmail.com>
2024-05-09 13:38:52 +0700

    setup route navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {MainRouter} from './router';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.description}>Hello </Text>
-    </View>
-  );
+  return <MainRouter />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  description: {
-    fontSize: 20,
-    color: '#000',
-    fontFamily: 'poppins_bold',
-  },
-});
