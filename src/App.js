e2eb8bc2d8df99b275f6ae3eac6138d1ded5ac5e
Muh. S. Roziki <muh.roziki@gmail.com>
2024-05-09 12:10:26 +0700

    change default load screen

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.description}>Hello </Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  description: {
+    fontSize: 20,
+    color: '#000',
+    fontFamily: 'poppins_bold',
+  },
+});
