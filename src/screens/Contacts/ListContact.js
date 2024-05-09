c4ef3e28f501b31ffc18aa9ea3f36f3a8df957a7
Muh. S. Roziki <muh.roziki@gmail.com>
2024-05-09 17:34:08 +0700

    create component list item

---
+++ src/screens/Contacts/ListContact.js
@@ -0,0 +1,56 @@
+import React, {useEffect} from 'react';
+import {FlatList, StatusBar, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {HeaderHome, ItemList} from '../../components';
+
+// styles
+import {styles} from './ListContact.style';
+import {color} from '../../styles';
+import {getListContact} from '../../store/actions';
+
+const ListContact = () => {
+  const dispatch = useDispatch();
+  const {data, message, loading} = useSelector(state => state.getAllContact);
+
+  useEffect(() => {
+    dispatch(getListContact());
+  }, [dispatch]);
+
+  const _renderItem = ({item, index}) => {
+    const fullName = `${item.firstName} ${item.lastName}`;
+    return (
+      <ItemList
+        id={item.id}
+        fullName={fullName}
+        age={item.age}
+        imgUrl={item.photo}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={color.secondary} barStyle={'dark-content'} />
+      <FlatList
+        data={data}
+        renderItem={_renderItem}
+        keyExtractor={(item, index) => item.id}
+        contentContainerStyle={styles.contentContainerStyle}
+        ListHeaderComponent={
+          <HeaderHome title={'Contacts'} count={data.length} />
+        }
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>
+              {loading ? 'Loading...' : message}
+            </Text>
+          </View>
+        }
+        ItemSeparatorComponent={<View style={styles.separator} />}
+        removeClippedSubviews={true}
+      />
+    </View>
+  );
+};
+
+export default ListContact;
---
+++ src/screens/Contacts/ListContact.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import {color} from '../../styles';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: color.white,
+  },
+  separator: {
+    height: 4,
+  },
+  contentContainerStyle: {paddingBottom: 16},
+});
